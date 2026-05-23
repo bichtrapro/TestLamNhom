4383dfc9eb1a63ec78d6231b70e7a32790a5d80a
traltb <traltb@fpt.edu.vn>
2026-05-23 18:01:11 +0700

    Test lam Nhom

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginForm from './components/LoginForm';
+import { AuthProvider } from './contexts/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import ProtectedRoute from './routes/ProtectedRoute';
+import ExpensesDashboard from './components/ExpensesDashboard';
+
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+      <Routes>
+ <Route path="/login" element={<LoginForm />} /> 
+        {/* Các route khác của bạn sẽ được đặt ở đây */}
+        <Route path= "/" element={
+          <ProtectedRoute>
+              <ExpensesDashboard />
+          </ProtectedRoute> 
+          } />
+        </Routes>
+     
+
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
